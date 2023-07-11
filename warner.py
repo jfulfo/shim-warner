1ce3067de47b2f5469557edc8c0785a8b6ae3e2a
jfulfo <jake.fulfo@gmail.com>
2023-07-11 00:52:12 -0400

    added buttons

--- warner.py
+++ warner.py
@@ -2,7 +2,7 @@ import os
 import logging
 import interactions
 from dotenv import load_dotenv
-from interactions import Client, slash_command, slash_option, OptionType, SlashContext, Intents, User
+from interactions import Client, slash_command, slash_option, OptionType, SlashContext, Intents, User, Button, ButtonStyle, ActionRow, spread_to_rows, listen
 
 load_dotenv()
 logging.basicConfig()
@@ -12,8 +12,11 @@ cls_log.setLevel(logging.DEBUG)
 TOKEN = os.getenv("TOKEN")
 bot = Client(intents=Intents.DEFAULT)
 
+hitters = []
+sop_setters = []
+keep_setters = []
 
-@slash_command(name="notif", description="notify a user")
+@slash_command(name="notif", description="Notify a user")
 @slash_option(
     name="user",
     description="The user to notify",
@@ -38,7 +41,38 @@ async def notif(ctx: SlashContext, user: User, text: str, times: int = 5):
     for _ in range(times):
         await user.send(f"{text}")
 
+components: list[ActionRow] = spread_to_rows(
+    Button(style=ButtonStyle.PRIMARY,custom_id="hitters",label="Hitter",disabled=False),
+    Button(style=ButtonStyle.PRIMARY,custom_id="sop_setters",label="SOP Setter",disabled=False),
+    Button(style=ButtonStyle.PRIMARY,custom_id="keep_setters",label="Keep Setter",disabled=False)
+)
+
+@listen()
+async def on_component(event):
+    ctx = event.ctx
+    if ctx.author.user.mention in hitters:
+        hitters.remove(ctx.author.user.mention)
+    elif ctx.author.user.mention in sop_setters:
+        sop_setters.remove(ctx.author.user.mention)
+    elif ctx.author.user.mention in keep_setters:
+        keep_setters.remove(ctx.author.user.mention)
+
+    match ctx.custom_id:
+        case "hitters":
+            hitters.append(ctx.author.user.mention)
+        case "sop_setters":
+            sop_setters.append(ctx.author.user.mention)
+        case "keep_setters":
+            keep_setters.append(ctx.author.user.mention)
+
+    await ctx.edit_origin(content=f"**Please react to your respective button if you are online and available:**\n\nHitter: {', '.join(hitters)}\nSOP Setter: {', '.join(sop_setters)}\nKeep Setter: {', '.join(keep_setters)}", components=components)
+
+@slash_command(name="rollcall", description="Perform a rollcall")
+async def rollcall(ctx: SlashContext):
+    response = await ctx.respond("**Please react to your respective button if you are online and available:**\n\nHitter:\nSOP Setter:\nKeep Setter:", components=components)
+
 if __name__ == "__main__":
     print("Starting bot...")
     bot.start(TOKEN)
 
+    
